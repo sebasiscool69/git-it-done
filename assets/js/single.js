26c2627063f4fa6fa1cd62de335f419d70a57202
sebz <bonillas175@gmail.com>
2021-07-27 12:15:44 -0400

    list-issues done

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,32 +1,44 @@
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
-var getRepoIssues = function(repo) {
-    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+  
+    if (repoName) {
+      // display repo name on the page
+      repoNameEl.textContent = repoName;
+  
+      getRepoIssues(repoName);
+    } else {
+      // if no repo was given, redirect to the homepage
+      document.location.replace("./index.html");
+    }
+  };
 
+  var getRepoIssues = function(repo) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+  
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
-        // request was successful
-        if (response.ok) {
-            response.json().then(function(data) {
-              displayIssues(data);
-          
-              // check if api has paginated issues
-              if (response.headers.get("Link")) {
-                var linkEl = document.createElement("a");
-                linkEl.textContent = "See More Issues on GitHub.com";
-                linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
-                linkEl.setAttribute("target", "_blank");
-              
-                // append to warning container
-                limitWarningEl.appendChild(linkEl);
-            }
-            });
+      // request was successful
+      if (response.ok) {
+        response.json().then(function(data) {
+          displayIssues(data);
+  
+          // check if api has paginated issues
+          if (response.headers.get("Link")) {
+            displayWarning(repo);
           }
-        else {
-          alert("There was a problem with your request!");
-        }
-      });
-    console.log(repo);
+        });
+      } else {
+        // if not successful, redirect to homepage
+        document.location.replace("./index.html");
+      }
+    });
   };
 
 var displayIssues = function(issues) {
@@ -76,4 +88,4 @@ var displayIssues = function(issues) {
         limitWarningEl.appendChild(linkEl);
     };
   
-getRepoIssues("facebook/react");
+getRepoName();
